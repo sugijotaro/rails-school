4403763ce82ca9a7eafe93072a30e49b08068d50
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-12-11 20:11:24 +0900

    create Course controller, view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'faculties/show'
   resources :faculties, only: [ :show ], to: 'faculties#show'
   resources :students
+  resources :courses, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
