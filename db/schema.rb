ff7720baca7f499f869a72d2556170771f280a2b
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-12-11 18:44:59 +0900

    add grade

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_11_094158) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_11_094337) do
   create_table "faculties", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_11_094158) do
     t.integer "faculty_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "grade"
   end
 
 end
