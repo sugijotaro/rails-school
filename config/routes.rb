89dc625201c84b25026fadfa266d3ab29b16344f
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-12-11 19:59:22 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'faculties/show'
+  resources :faculties, only: [ :show ], to: 'faculties#show'
   resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
